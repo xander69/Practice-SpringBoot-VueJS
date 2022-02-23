035f4090f725087fff4831387870350325279151
Alexander Shakhov <xander.shahov@gmail.com>
2022-02-24 00:34:21 +0300

    created components for message list

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,7 +1,27 @@
-Vue.createApp({
+const app = Vue.createApp({
+    template: '<MessageList :messages="messages"/>',
     data() {
         return {
-            message: 'Hello Vue!'
+            messages: [
+                {id: '1', text: 'First message'},
+                {id: '2', text: 'Second message'},
+                {id: '3', text: 'Third message'}
+            ]
         }
     }
-}).mount('#app')
+})
+
+app.component('MessageList', {
+    props: ['messages'],
+    template:
+        '<div>' +
+        '<MessagesRow v-for="message in messages" :key="message.id" :message="message"/>' +
+        '</div>'
+})
+
+app.component('MessagesRow', {
+    props: ['message'],
+    template: '<div>{{ message.id }}. <i>{{ message.text }}</i></div>'
+});
+
+app.mount('#app')
